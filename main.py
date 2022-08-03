2b14feff067ecfa5fd88ff170c8298d9b0843a19
Nijat Hamidov <hamidov9jat@gmail.com>
2022-08-03 16:42:11 +0400

    example images are added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Get command line arguments
 # Check the folders
-
+import pathlib
 from sys import argv
 from pathlib import Path
 
@@ -10,7 +10,9 @@ n = len(argv)
 
 if n == 3:
     # check for the folder name
-    source_folder = argv[1]
-    destination_folder = argv[2]
+    source_folder = pathlib.Path(argv[1])
+    destination_folder = pathlib.Path(argv[2])
+
+
 
 
