79f798ebb84a43d645f7f747928344cd7c8f6073
Nijat Hamidov <hamidov9jat@gmail.com>
2022-08-03 17:41:13 +0400

    created extract images function

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 # Get command line arguments
 # Check the folders
 import pathlib
+import sys
 from sys import argv
-from pathlib import Path
+import pathlib as pth
+import os
+import re
 
-#get cmd arguments
+# source = pth.Path('./img_dst')
+#
+# jpegs = re.compile(r'^(.+)\.(jpg|jpeg)$')
 
-n = len(argv)
+# files = source.iterdir()
+# for full_image_name in files:
+#     # print(full_image_name)
+#     image_name = jpegs.fullmatch(full_image_name.name)
+#     # print(full_image_name.is_file())
+#     # print(pth.PurePath(source).name)
+#     if image_name is not None:
+#         print(image_name.group(1))
+    # print(full_image_name.name)
 
-if n == 3:
-    # check for the folder name
-    source_folder = pathlib.Path(argv[1])
-    destination_folder = pathlib.Path(argv[2])
 
+def image_names(img_format: str, directory='.'):
+    match img_format.lower():
+        case 'jpg' | 'jpeg':
+            image_regex = re.compile(r'^(.+)\.(jpg|jpeg)$')
+        case 'png':
+            image_regex = re.compile(r'^(.+)\.png$')
+        case _:
+            print(f'{img_format=} is not supported!')
+            sys.exit(1)
+    source = pth.Path(directory)
+    files = filter(pth.Path.is_file, source.iterdir())
+    for full_image_name in files:
+        image_name = image_regex.fullmatch(full_image_name.name)
+        if image_name is not None:
+            yield image_name.group(1)
 
-
-
+print(list(image_names('jpg', './img_src')))
